bab7722e022b78db517a2f7011a71f3eedc0a7fd
Ando Nary <ando.nary@gmail.com>
2021-10-17 13:24:14 +0200

    groundfactory unit

---
+++ tests/makeUnits.test.ts
@@ -0,0 +1,63 @@
+class GroundFactory {
+    async makeUnit(name: UnitName): Promise<Unit> {
+        if (!Object.values(UnitName).includes((name as UnitName))) throw new Error();
+        return {
+            name
+        }
+    }
+}
+
+interface Unit {
+    name: string;
+}
+
+function isUnit(unit: Unit): unit is Unit {
+    return (unit as Unit).name !== void 0;
+}
+
+enum UnitName {
+    infantry='Infantry',
+    mech='Mech'
+}
+
+describe('Make units', () => {
+    test('it should create an Unit called Infantry', async () => {
+        // Arrange
+        const groundFactory = new GroundFactory();
+
+        // Act
+        const infantry: Unit = await groundFactory.makeUnit(UnitName.infantry);
+
+        // Assert
+        expect(isUnit(infantry)).toBeTruthy();
+        expect(infantry.name).toEqual(UnitName.infantry);
+    });
+
+    test('it should create an Unit called Mech', async () => {
+        // Arrange
+        const groundFactory = new GroundFactory();
+
+        // Act
+        const mech: Unit = await groundFactory.makeUnit(UnitName.mech);
+
+        // Assert
+        expect(isUnit(mech)).toBeTruthy();
+        expect(mech.name).toEqual(UnitName.mech);
+    });
+
+    test('it should not create an unknown unit', async () => {
+        // Arrange
+        const groundFactory = new GroundFactory();
+        let error;
+
+        // Act
+        try {
+            await groundFactory.makeUnit("Swordman" as UnitName);
+        } catch (e) {
+            error = e;
+        }
+
+        // Assert
+        expect(error).toBeDefined();
+    });
+});
