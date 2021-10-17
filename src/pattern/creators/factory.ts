c00d5699f6ba45abfcb300830a93c678a7512a80
Ando Nary <ando.nary@gmail.com>
2021-10-17 17:30:57 +0200

    emerged factory method

--- src/pattern/creators/factory.ts
+++ src/pattern/creators/factory.ts
@@ -2,9 +2,14 @@ import {UnitName} from "../../utils/enums/unitName";
 import {Unit} from "../products/unit";
 
 export abstract class Factory {
-    abstract makeUnit(name: UnitName): Promise<Unit>
+    async makeUnit(name: UnitName): Promise<Unit> {
+        if (this.isInvalidUnit(name)) throw new Error();
+        return this.factoryMethod(name);
+    }
 
     isInvalidUnit(name: UnitName) {
         return !Object.values(UnitName).includes((name as UnitName));
     }
+
+    abstract factoryMethod(name: UnitName): Promise<Unit>
 }
--- src/pattern/creators/groundFactory.ts
+++ src/pattern/creators/groundFactory.ts
@@ -3,10 +3,7 @@ import {UnitName} from "../../utils/enums/unitName";
 import {Unit} from "../products/unit";
 
 export class GroundFactory extends Factory {
-    async makeUnit(name: UnitName): Promise<Unit> {
-        if (this.isInvalidUnit(name)) throw new Error();
-        return {
-            name
-        }
+    factoryMethod(name: UnitName): Promise<Unit> {
+        return Promise.resolve({ name });
     }
 }
